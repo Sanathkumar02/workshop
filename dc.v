48b30c92059131f6ccfaac47c32276486e95e386
Sanathkumar02 <sanathkumar230302@gmail.com>
2026-05-01 17:48:52 +0530

    Update design configuration for FIFO implementation

--- dc.v
+++ dc.v
@@ -1,27 +1,32 @@
-set target_library ./dc/ref/models/saed90nm_typ_ht.db
-       set link_library ./dc/ref/models/saed90nm_typ_ht.db
-       set $RTL_SOURCE_FILES ./full_adder.v
-       set RTL_SOURCE_FILES ./full_adder.v
-       set DESIGN_NAME full_adder
+set target_library ./../ref/lib/stdcell_rvt/saed32rvt_tt0p78vn40c.db 
+
+      set link_library ./../ref/lib/stdcell_rvt/saed32rvt_tt0p78vn40c.db
+       
+       set RTL_SOURCE_FILES ./../Design/fifo.v
+
+	set DESIGN_NAME fifo
+
        define_design_lib WORK -path ./WORK
        set_app_var hdlin_enable_hier_map true
        analyze -format verilog ${RTL_SOURCE_FILES}
        elaborate ${DESIGN_NAME}
       link
-      current_design
-     read_sdc ./full_adder.sdc
-      vi full_adder.sdc
-      read_sdc ./full_adder.sdc
-      report_timing -delay_type min
-     report_timing -delay_type max
-      start_gui
-      compile
+      
+current_design
+    
+read_sdc ./../Design/fifo.sdc
+
+      
+      
+      
       set_verification_top
       compile
       report_timing -delay_type max
       report_timing -delay_type min
       compile_ultra
+      
       report_timing -delay_type min
+      
       report_timing -delay_type max
       write -format verilog -hierarchy -output ./
       write -format verilog -hierarchy -output ./full_adder_netlist.v
